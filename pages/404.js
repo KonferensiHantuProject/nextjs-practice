d67c2c9353fec064bda8021e24339d408ad727f6
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2023-09-23 18:23:29 +0700

    Redirect

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,23 @@
 import Link from "next/link";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const NotFound = () => {
+    // Router
+    const router = useRouter()
+
+    // Fired When open page
+    useEffect( () => {
+
+        // Redirect
+       setTimeout( () => {
+
+        // To Home
+        router.push('/')
+
+       }, 3000)
+    }, [])
+
     return ( 
         <div className="not-found">
             <h1>Oops....</h1>
