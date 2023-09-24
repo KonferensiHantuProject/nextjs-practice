721a6ad2c5e07a3b053afd26ebf66e9508c12e57
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2023-09-24 18:47:01 +0700

    Data Fetching

--- pages/bone/index.js
+++ pages/bone/index.js
@@ -1,7 +1,32 @@
-const Bone = () => {
+import styles from '../../styles/Bone.module.css';
+
+// Run when component is rendered
+export const getStaticProps = async () => {
+    // Return Object
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+
+    // Data
+    const data = await res.json();
+
+    // Return Data
+    return {
+        props: {
+            bones: data
+        }
+    }
+}
+
+const Bone = ({ bones }) => {
     return ( 
         <div>
-            <h1>Bone</h1>
+            <h1>Bone List</h1>
+            {bones.map(bone => (
+                <div key={bone.id}>
+                    <a className={styles.single}>
+                        <h3>{bone.name}</h3>
+                    </a>
+                </div>
+            ))}
         </div>
      );
 }
