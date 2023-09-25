9416ba670a55a9bb740f706563bb2b576ce4ce0f
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2023-09-25 21:22:02 +0700

    Dynamic Routes

--- pages/bone/[id].js
+++ pages/bone/[id].js
@@ -1,3 +1,18 @@
+export const getStaticPath = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+
+    const paths = data.map(bone => {
+        return {
+            params: { id: bone.id.toString() }
+        }
+    })
+    return {
+        paths,
+        fallback: false
+    }
+}
+
 const Details = () => {
     return ( 
         <div>
