6c8c65bf034f2070c1dcf807e6f8b6e81a93de53
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2023-09-25 21:29:01 +0700

    Updates

--- pages/bone/[id].js
+++ pages/bone/[id].js
@@ -1,4 +1,4 @@
-export const getStaticPath = async () => {
+export const getStaticPaths = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/users');
     const data = await res.json();
 
@@ -13,10 +13,25 @@ export const getStaticPath = async () => {
     }
 }
 
-const Details = () => {
+export const getStaticProps = async (context) => {
+    const id = context.params.id
+    const res = await fetch('https://jsonplaceholder.typicode.com/users/' + id);
+    const data = await res.json();
+
+    return {
+        props: {
+            bone: data
+        }
+    }
+}
+
+const Details = ({ bone }) => {
     return ( 
         <div>
-            <h1>Halaman Detail</h1>
+            <h1>{ bone.name }</h1>
+            <p>{ bone.email }</p>
+            <p>{ bone.website }</p>
+            <p>{ bone.address.city }</p>
         </div>
     );
 }
